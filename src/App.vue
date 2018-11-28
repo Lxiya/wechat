e0267aafca33bb59be2231d8ebe8c724cd773cca
Lxiya <1318961003@qq.com>
2018-11-28 17:20:17 +0800

    banner主要商品
    banner主要商品

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
       <router-view></router-view>
     </section>
 
+    <div style="height:2000px"></div>
+
     <!-- 底部导航 -->
     <jdh-footer></jdh-footer>
   </div>
@@ -12,17 +14,20 @@
 
 <script>
 /* import components */
-import Footer from './components/common/footer/Footer'
+import Footer from "./components/common/footer/Footer";
 
 export default {
-	name: 'App',
-	components: {
-		'jdh-footer': Footer
-	}
-}
+  name: "App",
+  components: {
+    "jdh-footer": Footer
+  }
+};
 </script>
 
 <style lang="stylus">
+h1
+  margin 0
+  padding 0
 *
   box-sizing border-box
 body
@@ -33,5 +38,12 @@ ul, ol
   list-style none
   margin 0
   padding 0
+h1.item-name
+  font-size 12px
+  font-weight normal
+  color #666666
+span.item-price
+  font-size 12px
+  color #ff1301
 </style>
 
